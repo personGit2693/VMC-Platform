470072754f97a02eddd7032d6243bd4fb91826ee
Rogiero <Cruzjr.rogelio@gmail.com>
2024-10-11 15:38:20 +0800

    Setup Posts Page

---
+++ Module Posting/Pages/Page_Posts.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="../Styles/Page_Posts.css">
+	<title>VMC Posts</title>
+</head>
+<body>
+	<div class="rolayout_RoClass">
+		<div class="sidemecha_RoClass darkMode">
+			<div class="sidemechaCon_RoClass">
+				My Content
+			</div>
+		</div>
+		<div class="mainmecha_RoClass darkMode">
+			<div class="mainmechaCon_RoClass layoutOneHead_RoClass darkMode">
+				<div class="burger_RoClass"><img class="burgerIcon_RoClass"></div>
+
+				<!--Choose header style-->								
+				<div class="projTitleAndUserLogin_RoClass">
+					<div class="projTitleTxt_RoClass">VMC Platform</div>
+					<div class="userLogin_RoClass">
+						<div class="userLoginIconWrap_RoClass"><img src="../../src/avataricon.png"></div>
+						<div class="userLoginDetails_RoClass">Account Details</div>
+					</div>
+				</div>
+				<!--Choose header style-->
+			</div> 
+			<div class="mainmechaCon_RoClass">
+				My Content
+			</div>				
+		</div> 
+	</div>
+
+	<script type="text/javascript" src="../../Rogrid/Scripts/RogridNodeLayOneNavScript.js"></script>
+</body>
+</html>
