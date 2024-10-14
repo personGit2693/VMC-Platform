9806848af90de4c531703901e5ea52dd088609ea
Rogiero <Cruzjr.rogelio@gmail.com>
2024-10-14 16:21:51 +0800

    Incomplete Pre Layout

--- Module Posting/Pages/Page_Posts.php
+++ Module Posting/Pages/Page_Posts.php
@@ -8,27 +8,32 @@
 </head>
 <body>
 	<div class="rolayout_RoClass">
-		<div class="sidemecha_RoClass darkMode">
+		<div class="sidemecha_RoClass lightMode">
 			<div class="sidemechaCon_RoClass">
 				My Content
 			</div>
 		</div>
-		<div class="mainmecha_RoClass darkMode">
-			<div class="mainmechaCon_RoClass layoutOneHead_RoClass darkMode">
+		<div class="mainmecha_RoClass lightMode">
+			<div class="mainmechaCon_RoClass layoutOneHead_RoClass lightMode">
 				<div class="burger_RoClass"><img class="burgerIcon_RoClass"></div>
 
-				<!--Choose header style-->								
+				<!--Header-->
 				<div class="projTitleAndUserLogin_RoClass">
 					<div class="projTitleTxt_RoClass">VMC Platform</div>
 					<div class="userLogin_RoClass">
-						<div class="userLoginIconWrap_RoClass"><img src="../../src/avataricon.png"></div>
+						<div class="userLoginIconWrap_RoClass"><img src="../../src/MySampleDp.png"></div>
 						<div class="userLoginDetails_RoClass">Account Details</div>
 					</div>
 				</div>
-				<!--Choose header style-->
-			</div> 
-			<div class="mainmechaCon_RoClass">
-				My Content
+				<!--Header-->
+			</div>			
+
+			<div class="mainmechaCon_RoClass" id="pageContentsWrap">
+				<!--Featured wrap for smart only-->
+				<div class="mainmechaCon_RoClass" id="featuredWrap">
+					
+				</div>
+				<!--Featured wrap for smart only-->
 			</div>				
 		</div> 
 	</div>
