e504a5ae67e1536bb28f3cc2014f5e4f35ee66bd
Rogiero <Cruzjr.rogelio@gmail.com>
2024-10-22 16:20:42 +0800

    Smart only features controller was added
    Just finish the font and text design.

--- Module Posting/Pages/Page_Posts.php
+++ Module Posting/Pages/Page_Posts.php
@@ -16,11 +16,11 @@
 		<div class="mainmecha_RoClass lightMode">
 			<!--Page Header and Burger-->
 			<div class="mainmechaCon_RoClass layoutOneHead_RoClass lightMode">
-				<!---Burger-->
+				<!--Burger-->
 				<div class="burger_RoClass"><img class="burgerIcon_RoClass"></div>
-				<!---Burger-->
+				<!--Burger-->
 
-				<!---Header-->
+				<!--Header-->
 				<div class="projTitleAndUserLogin_RoClass">
 					<div class="projTitleTxt_RoClass">VMC Platform</div>
 					<div class="userLogin_RoClass">
@@ -28,24 +28,52 @@
 						<div class="userLoginDetails_RoClass">Account Details</div>
 					</div>
 				</div>
-				<!---Header-->
+				<!--Header-->
 			</div>
 			<!--Page Header and Burger-->			
 
 
 			<!--Page Body-->
 			<div class="mainmechaCon_RoClass" id="pageContentsWrap">
-				<!---Left Side or Contents Area-->
+				<!--Left Side or Contents Area-->
 				<div class="mainmechaCon_RoClass" id="mainContentWrap">
-					right side
-				</div>
-				<!---Left Side or Contents Area-->
+					<!--Smart only features controller-->
+					<div class="mainmechaCon_RoClass" id="featuresControlsWrap">
+						<!--Event feature controller-->
+						<div class="mainmechaCon_RoClass featuresControls">							
+							<div class="mainmechaCon_RoClass featuresIcon"></div>
+							<div class="mainmechaCon_RoClass featuresTextWrap">
+								<div class="mainmechaCon_RoClass featuresTextTitle">Event</div>
+								<div class="mainmechaCon_RoClass featuresTextDefinition">lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum</div>
+							</div>							
+						</div>	
+						<!--Event feature controller-->
 
-				<!---Right Side or Chat Area-->
-				<div class="mainmechaCon_RoClass" id="chatContentWrap">
+						<!--Event feature controller-->
+						<div class="mainmechaCon_RoClass featuresControls">							
+							<div class="mainmechaCon_RoClass featuresIcon"></div>
+							<div class="mainmechaCon_RoClass featuresTextWrap">
+								<div class="mainmechaCon_RoClass featuresTextTitle">Event</div>
+								<div class="mainmechaCon_RoClass featuresTextDefinition">lorem ipsum lorem ipsum lorem ipsum</div>
+							</div>							
+						</div>	
+						<!--Event feature controller-->					
+					</div>
+					<!--Smart only features controller-->
+
+					<!--Page Features-->
+					<div class="mainmechaCon_RoClass" id="featuresWrap">
+						Page features
+					</div>
+					<!--Page Features-->
+				</div>
+				<!--Left Side or Contents Area-->
+
+				<!--Right Side or Chat Area-->
+				<div class="mainmechaCon_RoClass lightMode" id="chatContentWrap">
 					Chat side
 				</div>
-				<!---Right Side or Chat Area-->
+				<!--Right Side or Chat Area-->
 			</div>
 			<!--Page Body-->				
 		</div> 
