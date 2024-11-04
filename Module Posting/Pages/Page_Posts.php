b2f5843a0a496ab7edd892e4bcbac71822766c17
Rogiero <Cruzjr.rogelio@gmail.com>
2024-11-04 16:33:03 +0800

    Polishing events section

--- Module Posting/Pages/Page_Posts.php
+++ Module Posting/Pages/Page_Posts.php
@@ -8,14 +8,14 @@
 </head>
 <body>
 	<div class="rolayout_RoClass">
-		<div class="sidemecha_RoClass darkMode">
+		<div class="sidemecha_RoClass lightMode">
 			<div class="sidemechaCon_RoClass">
 				My Content
 			</div>
 		</div>
-		<div class="mainmecha_RoClass darkMode">
+		<div class="mainmecha_RoClass lightMode">
 			<!--Page Header and Burger-->
-			<div class="mainmechaCon_RoClass layoutOneHead_RoClass darkMode">
+			<div class="mainmechaCon_RoClass layoutOneHead_RoClass lightMode">
 				<!--Burger-->
 				<div class="burger_RoClass"><img class="burgerIcon_RoClass"></div>
 				<!--Burger-->
@@ -53,8 +53,8 @@
 						<div class="mainmechaCon_RoClass featuresControls">							
 							<div class="mainmechaCon_RoClass featuresIcon" style="--featureIconSrc:url('../../src/Committees-White.png')"></div>
 							<div class="mainmechaCon_RoClass featuresTextWrap">
-								<div class="mainmechaCon_RoClass featuresTextTitle darkMode">Committees</div>
-								<div class="mainmechaCon_RoClass featuresTextDefinition darkMode">lorem ipsum lorem ipsum lorem ipsum</div>
+								<div class="mainmechaCon_RoClass featuresTextTitle lightMode">Committees</div>
+								<div class="mainmechaCon_RoClass featuresTextDefinition lightMode">lorem ipsum lorem ipsum lorem ipsum</div>
 							</div>							
 						</div>	
 						<!--Committees feature controller-->
@@ -63,30 +63,96 @@
 						<div class="mainmechaCon_RoClass featuresControls">							
 							<div class="mainmechaCon_RoClass featuresIcon" style="--featureIconSrc:url('../../src/Pin-White.png')"></div>
 							<div class="mainmechaCon_RoClass featuresTextWrap">
-								<div class="mainmechaCon_RoClass featuresTextTitle darkMode">Pinned Post</div>
-								<div class="mainmechaCon_RoClass featuresTextDefinition darkMode">lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</div>
+								<div class="mainmechaCon_RoClass featuresTextTitle lightMode">Pinned Post</div>
+								<div class="mainmechaCon_RoClass featuresTextDefinition lightMode">lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</div>
 							</div>							
 						</div>	
 						<!--Pinned Post feature controller-->					
 					</div>
 					<!--Smart only features controller-->
 
+
 					<!--Page Features-->
-					<div class="mainmechaCon_RoClass" id="featuresWrap" style="border: 2px solid blue; ">
+					<div class="mainmechaCon_RoClass" id="featuresWrap" style="border: 2px solid blue;">
+						<!--Events and Activities-->
+						<div class="mainmechaCon_RoClass perFeature" id="eventsSection" style="border: 2px solid orange; display: block;">
+							<div class="mainmechaCon_RoClass sectionsLabel lightMode" style="border: 2px solid gray;">Events and Activities</div>
+
+							<div class="mainmechaCon_RoClass sectionsWrap" id="eventsWrap" style="border: 2px solid gray;">
+								<!--Per event-->
+								<div class="mainmechaCon_RoClass events lightMode">
+									<img src="../../src/MySampleDp.png" class="eventsDp"/>
+									<div class="mainmechaCon_RoClass eventsDetailsWrap">
+										<div class="mainmechaCon_RoClass eventsTitle lightMode">ISO Accreditation</div>
+										<div class="mainmechaCon_RoClass eventsDetailsTxts">November 4, 2024</div>
+										<div class="mainmechaCon_RoClass eventsDetailsTxts">Motorpool building</div>
+										<div class="mainmechaCon_RoClass eventsDetailsTxts">By HEPO Unit</div>
+										<div class="mainmechaCon_RoClass eventsDetailsTxts">Posted by Juan Dela Cruz</div>
+										<div class="mainmechaCon_RoClass eventsOngoing">Ongoing</div>
+									</div>
+								</div>
+								<!--Per event-->
+
+								<!--Per event-->
+								<div class="mainmechaCon_RoClass events lightMode">
+									<img src="../../src/MySampleDp.png" class="eventsDp"/>
+									<div class="mainmechaCon_RoClass eventsDetailsWrap">
+										<div class="mainmechaCon_RoClass eventsTitle lightMode">Coffee Vendor is back!</div>
+										<div class="mainmechaCon_RoClass eventsDetailsTxts">November 5, 2024</div>
+										<div class="mainmechaCon_RoClass eventsDetailsTxts">Annex building</div>
+										<div class="mainmechaCon_RoClass eventsDetailsTxts">By HEPO Unit</div>
+										<div class="mainmechaCon_RoClass eventsDetailsTxts">Posted by Juan Dela Cruz</div>
+										<div class="mainmechaCon_RoClass eventsUpcoming">Upcoming</div>
+									</div>
+								</div>
+								<!--Per event-->
+
+								<!--Per event-->
+								<div class="mainmechaCon_RoClass events lightMode">
+									<img src="../../src/MySampleDp.png" class="eventsDp"/>
+									<div class="mainmechaCon_RoClass eventsDetailsWrap">
+										<div class="mainmechaCon_RoClass eventsTitle lightMode">Coffee Vendor is back!</div>
+										<div class="mainmechaCon_RoClass eventsDetailsTxts">November 5, 2024</div>
+										<div class="mainmechaCon_RoClass eventsDetailsTxts">Annex building</div>
+										<div class="mainmechaCon_RoClass eventsDetailsTxts">By HEPO Unit</div>
+										<div class="mainmechaCon_RoClass eventsDetailsTxts">Posted by Juan Dela Cruz</div>
+										<div class="mainmechaCon_RoClass eventsUpcoming">Upcoming</div>
+									</div>
+								</div>
+								<!--Per event-->
+
+								<!--Per event-->
+								<div class="mainmechaCon_RoClass events lightMode">
+									<img src="../../src/MySampleDp.png" class="eventsDp"/>
+									<div class="mainmechaCon_RoClass eventsDetailsWrap">
+										<div class="mainmechaCon_RoClass eventsTitle lightMode">Coffee Vendor is back!</div>
+										<div class="mainmechaCon_RoClass eventsDetailsTxts">November 5, 2024</div>
+										<div class="mainmechaCon_RoClass eventsDetailsTxts">Annex building</div>
+										<div class="mainmechaCon_RoClass eventsDetailsTxts">By HEPO Unit</div>
+										<div class="mainmechaCon_RoClass eventsDetailsTxts">Posted by Juan Dela Cruz</div>
+										<div class="mainmechaCon_RoClass eventsUpcoming">Upcoming</div>
+									</div>
+								</div>
+								<!--Per event-->
+							</div>
+						</div>
+						<!--Events and Activities-->
+
+
 						<!--What is new posts-->
-						<div class="mainmechaCon_RoClass" id="whatIsNewSection" style="border: 2px solid green;">
-							<div class="mainmechaCon_RoClass sectionsLabel darkMode">What's New</div>
+						<div class="mainmechaCon_RoClass perFeature" id="whatIsNewSection" style="border: 2px solid orange; display: none;">
+							<div class="mainmechaCon_RoClass sectionsLabel lightMode" style="border: 2px solid gray;">What's New</div>
 							
 							<!--Posted Section-->
-							<div class="mainmechaCon_RoClass" id="postsWrap">
+							<div class="mainmechaCon_RoClass sectionsWrap" id="postsWrap" style="border: 2px solid gray;">
 								<!--Per Post-->
-								<div class="mainmechaCon_RoClass posts darkMode">
+								<div class="mainmechaCon_RoClass posts lightMode">
 									<!--Post Header-->
 									<div class="mainmechaCon_RoClass postsHeader">
 										<div class="mainmechaCon_RoClass postsHeaderDetailsWrap">
 											<img class="postsDp" src="../../src/MySampleDp.png"/>
 											<div class="mainmechaCon_RoClass postsHeaderDetails">
-												<div class="mainmechaCon_RoClass postsEmpName darkMode">Juan Dela Cruz</div>
+												<div class="mainmechaCon_RoClass postsEmpName lightMode">Juan Dela Cruz</div>
 												<div class="mainmechaCon_RoClass postsCatNTimeWrap">
 													<div class="mainmechaCon_RoClass postsCatNTimeTxt">Category Here</div>
 													<div class="mainmechaCon_RoClass postsCatNTimeTxt">Just Now</div>
@@ -98,8 +164,8 @@
 
 									<!--Post Body-->
 									<div class="mainmechaCon_RoClass postsBody">
-										<div class="mainmechaCon_RoClass postsBodyDetails darkMode">lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</div>
-										<div class="mainmechaCon_RoClass postsTitle darkMode">Downfall of government salary</div>
+										<div class="mainmechaCon_RoClass postsBodyDetails lightMode">lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</div>
+										<div class="mainmechaCon_RoClass postsTitle lightMode">Downfall of government salary</div>
 										<div class="mainmechaCon_RoClass postsSeenDetailsWrap">
 											<div class="mainmechaCon_RoClass postsSeenIcon"></div>
 											<div class="mainmechaCon_RoClass postsSeenTxts">100</div>
@@ -110,13 +176,13 @@
 								<!--Per Post-->
 
 								<!--Per Post-->
-								<div class="mainmechaCon_RoClass posts darkMode">
+								<div class="mainmechaCon_RoClass posts lightMode">
 									<!--Post Header-->
 									<div class="mainmechaCon_RoClass postsHeader">
 										<div class="mainmechaCon_RoClass postsHeaderDetailsWrap">
 											<img class="postsDp" src="../../src/MySampleDp.png"/>
 											<div class="mainmechaCon_RoClass postsHeaderDetails">
-												<div class="mainmechaCon_RoClass postsEmpName darkMode">Juan Dela Cruz</div>
+												<div class="mainmechaCon_RoClass postsEmpName lightMode">Juan Dela Cruz</div>
 												<div class="mainmechaCon_RoClass postsCatNTimeWrap">
 													<div class="mainmechaCon_RoClass postsCatNTimeTxt">Category Here</div>
 													<div class="mainmechaCon_RoClass postsCatNTimeTxt">Just Now</div>
@@ -128,9 +194,9 @@
 
 									<!--Post Body-->
 									<div class="mainmechaCon_RoClass postsBody">
-										<div class="mainmechaCon_RoClass postsBodyDetails darkMode"><img src="./../../src/rally post.jpg"/></div>
-										<div class="mainmechaCon_RoClass postsBodyDetails darkMode">lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum</div>
-										<div class="mainmechaCon_RoClass postsTitle darkMode">Salary increase and corruption</div>
+										<div class="mainmechaCon_RoClass postsBodyDetails lightMode"><img src="./../../src/rally post.jpg"/></div>
+										<div class="mainmechaCon_RoClass postsBodyDetails lightMode">lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum</div>
+										<div class="mainmechaCon_RoClass postsTitle lightMode">Salary increase and corruption</div>
 										<div class="mainmechaCon_RoClass postsSeenDetailsWrap">
 											<div class="mainmechaCon_RoClass postsSeenIcon"></div>
 											<div class="mainmechaCon_RoClass postsSeenTxts">100</div>
@@ -138,28 +204,80 @@
 									</div>
 									<!--Post Body-->
 								</div>
-								<!--Per Post-->								
+								<!--Per Post-->
+
+								<!--Per Post-->
+								<div class="mainmechaCon_RoClass posts lightMode">
+									<!--Post Header-->
+									<div class="mainmechaCon_RoClass postsHeader">
+										<div class="mainmechaCon_RoClass postsHeaderDetailsWrap">
+											<img class="postsDp" src="../../src/MySampleDp.png"/>
+											<div class="mainmechaCon_RoClass postsHeaderDetails">
+												<div class="mainmechaCon_RoClass postsEmpName lightMode">Juan Dela Cruz</div>
+												<div class="mainmechaCon_RoClass postsCatNTimeWrap">
+													<div class="mainmechaCon_RoClass postsCatNTimeTxt">Category Here</div>
+													<div class="mainmechaCon_RoClass postsCatNTimeTxt">Just Now</div>
+												</div>
+											</div>											
+										</div>
+									</div>
+									<!--Post Header-->
+
+									<!--Post Body-->
+									<div class="mainmechaCon_RoClass postsBody">
+										<div class="mainmechaCon_RoClass postsBodyDetails lightMode"><img src="./../../src/rally post.jpg"/></div>
+										<div class="mainmechaCon_RoClass postsBodyDetails lightMode">lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum</div>
+										<div class="mainmechaCon_RoClass postsTitle lightMode">Salary increase and corruption</div>
+										<div class="mainmechaCon_RoClass postsSeenDetailsWrap">
+											<div class="mainmechaCon_RoClass postsSeenIcon"></div>
+											<div class="mainmechaCon_RoClass postsSeenTxts">100</div>
+										</div>
+									</div>
+									<!--Post Body-->
+								</div>
+								<!--Per Post-->	
+
+								<!--Per Post-->
+								<div class="mainmechaCon_RoClass posts lightMode">
+									<!--Post Header-->
+									<div class="mainmechaCon_RoClass postsHeader">
+										<div class="mainmechaCon_RoClass postsHeaderDetailsWrap">
+											<img class="postsDp" src="../../src/MySampleDp.png"/>
+											<div class="mainmechaCon_RoClass postsHeaderDetails">
+												<div class="mainmechaCon_RoClass postsEmpName lightMode">Juan Dela Cruz</div>
+												<div class="mainmechaCon_RoClass postsCatNTimeWrap">
+													<div class="mainmechaCon_RoClass postsCatNTimeTxt">Category Here</div>
+													<div class="mainmechaCon_RoClass postsCatNTimeTxt">Just Now</div>
+												</div>
+											</div>											
+										</div>
+									</div>
+									<!--Post Header-->
+
+									<!--Post Body-->
+									<div class="mainmechaCon_RoClass postsBody">
+										<div class="mainmechaCon_RoClass postsBodyDetails lightMode"><img src="./../../src/rally post.jpg"/></div>
+										<div class="mainmechaCon_RoClass postsBodyDetails lightMode">lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum</div>
+										<div class="mainmechaCon_RoClass postsTitle lightMode">Salary increase and corruption</div>
+										<div class="mainmechaCon_RoClass postsSeenDetailsWrap">
+											<div class="mainmechaCon_RoClass postsSeenIcon"></div>
+											<div class="mainmechaCon_RoClass postsSeenTxts">100</div>
+										</div>
+									</div>
+									<!--Post Body-->
+								</div>
+								<!--Per Post-->									
 							</div>
 							<!--Posted Section-->
 						</div>
-						<!--What is new posts-->
-
-						<!--Events-->
-						<div class="mainmechaCon_RoClass" id="eventsSection" style="border: 2px solid green;">
-							<div class="mainmechaCon_RoClass sectionsLabel darkMode">Events and Schedules</div>
-
-							<div class="mainmechaCon_RoClass" id="calendarWrap">
-								
-							</div>
-						</div>
-						<!--Events-->
+						<!--What is new posts-->						
 					</div>
 					<!--Page Features-->
 				</div>
 				<!--Left Side or Contents Area-->
 
 				<!--Right Side or Chat Area-->
-				<div class="mainmechaCon_RoClass darkMode" id="chatContentWrap" style="border: 2px solid red;">
+				<div class="mainmechaCon_RoClass lightMode" id="chatContentWrap" style="border: 2px solid red;">
 					Chat side
 				</div>
 				<!--Right Side or Chat Area-->
