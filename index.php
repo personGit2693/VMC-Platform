e9d0eb8a01a49f9793883432bc3673883bd4f167
Rogiero <Cruzjr.rogelio@gmail.com>
2024-11-25 16:38:08 +0800

    Continue on creating web app list section

--- index.php
+++ index.php
@@ -3,16 +3,75 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>	
-	<script type="text/javascript">
-		console.log();
-	</script>
+	<title>VMC Platform</title>	
+	<link rel="stylesheet" type="text/css" href="./Module Index/Styles/index.css">
 </head>
 <body>
-	<!--
-	<button onclick="controller_Btn_Login()">test hack</button>
+	<div class="rolayout_RoClass"> 
+		<div class="sidemecha_RoClass lightMode">
+			<div class="sidemechaCon_RoClass">
+				
+			</div>
+		</div>
+		<div class="mainmecha_RoClass lightMode">
+			<div class="mainmechaCon_RoClass layoutOneHead_RoClass lightMode">
+				<div class="burger_RoClass"><img class="burgerIcon_RoClass"></div>			
+				
+				<div class="projTitleAndUserLogin_RoClass">
+					<div class="projTitleTxt_RoClass">VMC Web Applications</div>
+					<div class="userLogin_RoClass">
+						<div class="userLoginIconWrap_RoClass"><img src="./src/MySampleDp.png"/></div>
+						<div class="userLoginDetails_RoClass">Juan Dela Cruz</div>
+					</div>
+				</div>
+			</div>
 
-	<script type="module" src="../VMC CSAT/Module Login/Client Side/Controller_Btn_Login.js"></script>
-	-->
+			<!--Greetings Area--> 
+			<div class="mainmechaCon_RoClass autoReponsiveSection lightMode" id="greetingSection">
+				<div class="mainmechaCon_RoClass" id="greetingWrap">
+					<img id="greetingUserImage" src="./src/MySampleDp.png" />
+						
+					<div class="mainmechaCon_RoClass" id="greetingUserDetailsWrap">
+						<div class="mainmechaCon_RoClass lightMode" id="greetingUserFName">Hi! welcome back, Juan!</div>
+						<div class="mainmechaCon_RoClass sub-textFColor" id="greetingUserEmpNo">Employee number 2024-1136</div>
+					</div>
+				</div>
+
+				<div class="mainmechaCon_RoClass" id="greetingWebApps"></div>
+			</div>
+			<!--Greetings Area--> 
+
+
+			<!--Banner-->				
+			<div class="mainmechaCon_RoClass autoReponsiveSection" id="bannerWrap">
+				<div class="mainmechaCon_RoClass" id="bannerDetailsWrap">
+					<div class="mainmechaCon_RoClass" id="bannerDetailsMainTxt">Valenzuela Medical Center Web Applications Center</div>
+					<div class="mainmechaCon_RoClass" id="bannerDetailsSubTxt">VMC Web apps are now accessible from any device with a web browser and can be updated centrally</div>
+					<a class="link-Btn" href="" id="bannerBtnGoToApp">Go to Systems</a>
+				</div>
+			</div>
+			<!--Banner-->
+
+
+			<!--List of web applications-->
+			<div class="mainmechaCon_RoClass autoReponsiveSection lightMode" id="webAppListSection" style="border: 2px solid yellow;">
+				<div class="mainmechaCon_RoClass sectionsLabel lightMode" style="border: 2px solid red;">List of Systems</div>
+				<div class="mainmechaCon_RoClass" id="webAppList" style="border: 2px solid red;">
+					<div class="mainmechaCon_RoClass perWebApp">
+						<div class="mainmechaCon_RoClass perWebAppIcon_LightMode"></div>
+						<div class="mainmechaCon_RoClass perWebAppName lightMode">System Name</div>
+					</div>
+				</div>
+			</div>
+			<!--List of web applications-->
+		</div> 
+	</div>
+
+
+	<!--JS Script-->
+	<!---Dependecies-->
+	<script type="text/javascript" src="./Rogrid/Scripts/RogridNodeLayOneNavScript_Index.js"></script>
+	<!---Dependecies-->	
+	<!--JS Script-->
 </body>
 </html>
